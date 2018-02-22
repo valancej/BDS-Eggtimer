11a637edd98847566aad39a06814fb1d329d0f74
Jeremy Valance <jvalance@pcm-jvalance-1.local>
2018-02-22 09:45:15 -0500

    added sleep

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import time
 import psycopg2
 import threading
 
-
-### Import config file
+# Import config file
 with open("config.yml", "r") as ymlfile:
     cfg = yaml.load(ymlfile)
 
-### Set Black Duck Hub variables from config.yml
+### 
+# Set Black Duck Hub variables from config.yml
 blackDuckHubHost = cfg['blackduck']['hubHost']
 blackDuckHubAuthToken = cfg['blackduck']['hubUserAuthToken']
 
@@ -20,33 +20,12 @@ hubUserId = "00000000-0000-0000-0001-000000000001"
 
 ###
 # Set postgres variables from config.yml
-###
 DATABASE_CONFIG = {
     'host': cfg['postgres']['host'],
     'dbname': cfg['postgres']['dbname'],
     'user': cfg['postgres']['user'],
     'password': cfg['postgres']['host']
 }
-
-
-# Hub Auth
-try:
-    hubConn = http.client.HTTPSConnection(blackDuckHubHost)
-    headers = {
-        'authorization': blackDuckHubAuthToken,
-        'cache-control': "no-cache",
-    }
-    hubConn.request("POST", "/api/tokens/authenticate", headers=headers)
-    res = hubConn.getresponse()
-    data = res.read().decode("utf-8")
-    auth_obj = json.loads(data)
-    bearerToken = auth_obj["bearerToken"]
-    print("Hub Authorization succeeded.")
-
-except Exception as e:
-    print("Cannot connect to Hub. Invalid token.")
-    print(e)
-
 
 # DB Connect
 try:
@@ -56,7 +35,6 @@ try:
     cursor = dbConn.cursor()
     print("Database connection succeeded.")
     cursor.execute("""CREATE TABLE IF NOT EXISTS notifications (id serial primary key, posted_date date not null, notification_id varchar(255) not null)""")
-    cursor.execute("INSERT INTO public.notifications (posted_date, notification_id) VALUES (%s, %s)", ('2018-01-22', 'fjdaklfjadkls'))
     cursor.execute("""SELECT * FROM notifications""")
     rows = cursor.fetchall()
     print(rows)
@@ -65,16 +43,30 @@ except Exception as e:
     print("Cannot connect to database. Invalid dbname, user or password.")
     print(e)
 
-
 # Get Policy overrides
-def getNewPolicyOverrideNotifications(bearerToken):
+def getNewPolicyOverrideNotifications():
 
-    threading.Timer(600.0, getNewPolicyOverrideNotifications, [bearerToken]).start()
-    print("Thread running")
+    try:
+        hubConn = http.client.HTTPSConnection(blackDuckHubHost)
+        headers = {
+            'authorization': blackDuckHubAuthToken,
+            'cache-control': "no-cache",
+        }
+        hubConn.request("POST", "/api/tokens/authenticate", headers=headers)
+        res = hubConn.getresponse()
+        data = res.read().decode("utf-8")
+        auth_obj = json.loads(data)
+        bearerToken = auth_obj["bearerToken"]
+        print("Hub Authorization succeeded.")
+
+    except Exception as e:
+        print("Cannot connect to Hub. Invalid token.")
+        print(e)
 
     authHeaders = {
         'authorization': "Bearer " + bearerToken
     }
+    
     hubConn.request("GET", "/api/v1/users/" + hubUserId + "/notifications?limit=100&offset=0&states=NEW&filter=notificationType%3Apolicy_override", headers=authHeaders)
 
     res = hubConn.getresponse()
@@ -93,5 +85,7 @@ def getNewPolicyOverrideNotifications(bearerToken):
         print(formattedTimeStamp)
 
 
-
-getNewPolicyOverrideNotifications(bearerToken)
+while True:
+    print("Process running.")
+    getNewPolicyOverrideNotifications()
+    time.sleep(86400)
